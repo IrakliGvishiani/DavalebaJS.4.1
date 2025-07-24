044390a7b4e9fec58525e2c0d6ed0dcc6db19eaf
Default <irakligvidhiani@gmail.com>
2025-07-24 17:26:34 +0400

    homework

---
+++ script.js
@@ -0,0 +1,63 @@
+////////////// 1.
+// let array = ["გიორგი","ნიკა","ლაშა","დათო","ზურა"]
+
+// let amount = 0
+// let count = 0
+
+// let nameN = 0
+
+
+// for(let i = 0;i<array.length;i++){
+
+//   array[i]=count
+//   count++
+   
+// }
+// console.log(count)
+
+////////////////////////////////////////////////////////
+
+///////////////////// 2.
+
+// let arrayy = [29,15,55,65,12,90,62,87]
+
+// let frst = arrayy[0]
+
+// for(let i = 0;i<arrayy.length;i++){
+//     if(frst>arrayy[i]){
+//         frst = arrayy[i]
+//     }
+// }
+
+// console.log(frst)
+
+////////////////////////////////////////////////////
+
+////////////////////////// 3.
+
+// let studentScore = [90,95,100,89,87,54,91,]
+
+// for(let i = 0;i<studentScore.length;i++){
+//     if(studentScore[i]>=90){
+//         console.log(studentScore[i])
+//     }
+// }
+
+//////////////////////////////////
+
+////////////////// 4.
+
+let notNumber = ["მაგიდა","სკამი","ჭაღი","დივანი","მაცივარი"]
+
+let count = 0
+let count1 = 0
+let smth = notNumber[0]
+
+for(let i = 0;i<smth.length;i++){
+
+  smth[i] = count
+  count++
+
+  
+}
+console.log(count)
